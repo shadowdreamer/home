96a0937f46a7e689786c6caa2d88f8f4ec476605
shadowdreamer <uzumaki@yeah.net>
2019-05-25 16:36:28 +0800

    redirect

--- src/index/views/Home.vue
+++ src/index/views/Home.vue
@@ -5,6 +5,8 @@
    </h1>
    links:
    <p><a href="/producer">producer page</a></p>
+   <p><a href="https://puzzle.dovahkiin.top/">puzzle.dovahkiin.top</a></p>
+   <p><a href="https://rankhime.dovahkiin.top/">rankhime.dovahkiin.top</a></p>
   </div>
 </template>
 
