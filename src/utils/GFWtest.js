a0d3da2d719bca4c305c669dac00154218fe8990
shadowdreamer <uzumaki@yeah.net>
2019-05-31 18:02:57 +0800

    Update GFWtest.js

--- src/utils/GFWtest.js
+++ src/utils/GFWtest.js
@@ -2,7 +2,7 @@ export default class {
     constructor() {
         return new Promise((resove, reject) => {
             const img = new Image()
-            img.src = 'https://www.google.com/images/logo.png'
+            img.src = 'https://lh3.googleusercontent.com/-5hRUWqlfk6Y/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rc_gVgfoyi72aFn65AuchWxlYFFGw.CMID/s32-c-mo/photo.jpg'
             img.onload = () => resove()
             img.onerror = () => reject()
         })
